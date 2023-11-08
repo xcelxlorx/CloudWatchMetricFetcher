2dbdad13b003fa517b80c4a9091173271a9bce1d
xcelxlorx <xcelxlorx@gmail.com>
2023-11-08 21:25:52 +0900

    feat: Modify file name

--- src/main/java/org/example/CloudWatch.java
+++ src/main/java/org/example/CloudWatch.java
@@ -1,7 +1,7 @@
 package org.example;
 
 import org.example.cloudwatch.MetricDataResponse;
-import org.example.cloudwatch.MetricFetcher;
+import org.example.cloudwatch.Ec2MetricFetcher;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,21 +11,21 @@ import static java.lang.System.*;
 public class CloudWatch {
     public static void main(String[] args) {
 
-        MetricFetcher metricFetcher = new MetricFetcher();
+        Ec2MetricFetcher ec2MetricFetcher = new Ec2MetricFetcher();
 
         List<MetricDataResponse> responses = new ArrayList<>();
 
-        responses.add(metricFetcher.getCPUUtilization());
-        responses.add(metricFetcher.getStatusCheckFailedSystem());
-        responses.add(metricFetcher.getStatusCheckFailedInstance());
-        responses.add(metricFetcher.getNetworkIn());
-        responses.add(metricFetcher.getNetworkOut());
-        responses.add(metricFetcher.getNetworkPacketsIn());
-        responses.add(metricFetcher.getNetworkPacketsOut());
-        responses.add(metricFetcher.getDiskReadOperations());
-        responses.add(metricFetcher.getDiskReads());
-        responses.add(metricFetcher.getDiskWriteOperations());
-        responses.add(metricFetcher.getDiskWrites());
+        responses.add(ec2MetricFetcher.getCPUUtilization());
+        responses.add(ec2MetricFetcher.getStatusCheckFailedSystem());
+        responses.add(ec2MetricFetcher.getStatusCheckFailedInstance());
+        responses.add(ec2MetricFetcher.getNetworkIn());
+        responses.add(ec2MetricFetcher.getNetworkOut());
+        responses.add(ec2MetricFetcher.getNetworkPacketsIn());
+        responses.add(ec2MetricFetcher.getNetworkPacketsOut());
+        responses.add(ec2MetricFetcher.getDiskReadOperations());
+        responses.add(ec2MetricFetcher.getDiskReads());
+        responses.add(ec2MetricFetcher.getDiskWriteOperations());
+        responses.add(ec2MetricFetcher.getDiskWrites());
 
         for (MetricDataResponse response : responses) {
             out.println("id=" + response.getId());
